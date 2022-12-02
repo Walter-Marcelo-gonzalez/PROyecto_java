b45a93a0cd8bd649b0994358aa1930a2e23b5720
Walter-Marcelo-Gonzalez <walterwalter1976@gmail.com>
2022-12-01 22:18:31 -0300

    subiendo nuevo proyecto

---
+++ nuevo_proyecto/src/main/java/paquete_walter/Controlador.java
@@ -0,0 +1,15 @@
+package paquete_walter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controlador {
+
+	@GetMapping("/")
+	public String Este() {	
+	
+		return ("hola mundo html desde otro proyecto..");
+}
+
+}
